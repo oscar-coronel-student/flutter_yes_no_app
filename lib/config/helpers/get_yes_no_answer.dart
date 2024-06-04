d40108ccb115d2dce10a405cc0939ba803f2dde1
Oscar Coronel <oscar.coronel@conauto.com.ec>
2024-06-04 17:06:22 -0500

    Crear carpeta config para los temas y helpers

---
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -0,0 +1,14 @@
+
+import 'package:yes_no_app/domain/entities/message.dart';
+
+
+class YesNoAnswer {
+
+  Future<Message> getAnswer(){
+
+    final Uri url = Uri.https('yesno.wtf', '/api');
+
+    return;
+  }
+
+}
